29869784898d1f2928452547dc13413278e1b102
萩原涼【マーケティング】(ryo hagiwara) <ryo_hagiwara@mates-system.com>
2021-06-09 16:45:40 +0900

    first commit

---
+++ panel.R
@@ -0,0 +1,26 @@
+# source("panel.R",echo = TRUE)とかで実行
+library(plm)
+
+# plmパッケージに付属するGrunfeldデータの読み込み
+# アメリカの有名企業に関するパネルデータ
+# firm:企業番号,year:西暦,inv:総投資,value:企業価値,capital:有形固定資産
+data("Grunfeld",package="plm")
+
+# もしcsvで読み込みたいなら以下コメントアウトを外す
+# どっちみちGrunfeldの内容は同じになります
+# Grunfeld <= read.csv("panel.csv")
+
+# データの最初の25行をみる
+head(Grunfeld,25)
+
+
+# 以下モデルを推定する
+# inv_it = α_i + β_1value_it + β2capital_it +_εit
+# model="pooling"はPooled OLSで推定
+# 引数はモデルの式、データ、モデル形式
+result1=plm(inv~value+capital,data=Grunfeld,model="pooling")
+
+summary(result1)
+
+result2=plm(inv~value+capital,data=Grunfeld,model="within")
+summary(result2)
